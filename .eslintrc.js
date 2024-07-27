cfca5b639bd3cdc907e322e6dbf3f35cc4c24944
“Husnain-Ashfaq39” <127616463+Husnain-Ashfaq39@users.noreply.github.com>
2024-07-27 11:36:40 +0500

    eslint error solved

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended'
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+      'react-hooks',
+    ],
+    rules: {
+      'react-hooks/exhaustive-deps': 'off',
+      // Add other rules as needed
+    },
+  };
+  
