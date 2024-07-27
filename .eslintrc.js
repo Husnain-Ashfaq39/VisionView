c766651385438621680e5c3e3b94770e69cc3546
“Husnain-Ashfaq39” <127616463+Husnain-Ashfaq39@users.noreply.github.com>
2024-07-27 11:45:59 +0500

    update build

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,17 @@ module.exports = {
     ],
     rules: {
       'react-hooks/exhaustive-deps': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+      'no-unused-vars': 'off',
+      'react/jsx-key': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/no-children-prop': 'off',
+      'no-case-declarations': 'off',
+      'no-extra-semi': 'off',
+      'react/no-find-dom-node': 'off',
+      'no-prototype-builtins': 'off'
       // Add other rules as needed
     },
   };
